5db564c137bfa4718f0b7fce8cfab6f1ebca13b4
rsemmler <rsemmelr@cisco.com>
2017-11-05 23:37:03 -0500

    .

--- secrets_format.py
+++ secrets_format.py
@@ -1,9 +1,7 @@
 # Maybe reintegrate this into secrets.py
 # will probably have to expend twitter_config to include the keys, with "" values.
 
-template = """from get_data import Member, Bill, Vote
-
-geocodio_key = '{}'
+template = """geocodio_key = '{}'
 propublica_header = {}
 
 # "consumer_key" is the API key. "Consumer_secret" is the API secret.
