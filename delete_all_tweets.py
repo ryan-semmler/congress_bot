b847423ebdd74029ffa1b797e31ad2e483b5087b
rsemmler <ryan.semmler@gmail.com>
2018-07-09 01:49:48 -0400

    simplify delete_all_tweets

--- delete_all_tweets.py
+++ delete_all_tweets.py
@@ -1,26 +1,14 @@
-import tweepy
-from config import twitter_config, handle, days_old_limit
+from get_data import get_api
+from config import handle, days_old_limit
 import time
 import datetime
 
 
-def get_api():
-    """
-    Creates tweepy api object
-    """
-    auth = tweepy.OAuthHandler(twitter_config['consumer_key'], twitter_config['consumer_secret'])
-    auth.set_access_token(twitter_config['access_token'], twitter_config['access_token_secret'])
-    api = tweepy.API(auth)
-    return api
-
-
-api = get_api()
-
-
-def delete_tweets(handle):
+def delete_tweets():
     total = 0
     date = time.localtime()
     now = datetime.date(date.tm_year, date.tm_mon, date.tm_mday)
+    api = get_api()
     while True:
         tweets = [post for post in api.user_timeline(handle) if (now - post.created_at.date()).days <= days_old_limit]
         total += len(tweets)
@@ -37,6 +25,6 @@ if __name__ == '__main__':
     response = input(f"If you continue, all tweets from the last {days_old_limit} day{'s' * (days_old_limit != 1)} "
                      f"will be deleted. Continue? (y/N): ")
     if 'y' in response.lower():
-        delete_tweets(handle)
+        delete_tweets()
     else:
         print("Cancelled. Did not delete tweets.")
